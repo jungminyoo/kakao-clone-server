362d8daacc7189a80130a5ebfc3e415e122f4f52
jungminyoo <yoojm4718@gmail.com>
2021-09-17 21:41:51 +0900

    Configured SocketIO and Admin-Panel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import "dotenv/config";
 import morgan from "morgan";
+import { Server } from "socket.io";
+import http from "http";
+import { instrument } from "@socket.io/admin-ui";
 
 const PORT = process.env.PORT;
 const app = express();
@@ -9,6 +12,23 @@ const logger = morgan("dev");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (_, res) => res.send("Home"));
+app.get("/*", (_, res) => res.send("404 Not Found"));
+
 const handleListening = () => console.log(`Server listening on port ${PORT}`);
 
-app.listen(PORT, handleListening);
+const httpServer = http.createServer(app);
+const ioServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+
+instrument(ioServer, {
+  auth: false,
+});
+
+ioServer.on("connection", (socket) => {});
+
+httpServer.listen(PORT, handleListening);
