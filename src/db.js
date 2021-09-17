6af940831941a1f0255cd38de1959b6c25b0226b
jungminyoo <yoojm4718@gmail.com>
2021-09-17 23:09:02 +0900

    Configured MongoDB Adapter of SocketIO

--- src/db.js
+++ src/db.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import ioServer from "./index";
+import { createAdapter } from "@socket.io/mongo-adapter";
 
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
@@ -6,8 +8,18 @@ mongoose.connect(process.env.DB_URL, {
 });
 
 const db = mongoose.connection;
+const IOCONNECTION = "socket.io-adapter";
 
-const handleOpen = () => console.log("Connected to DB!");
+const handleOpen = async () => {
+  console.log("Connected to DB!");
+  try {
+    await db.createCollection(IOCONNECTION, { capped: true, size: 1e6 });
+  } catch (e) {
+    console.log("Collection Already Exists");
+  }
+  const mongoCollection = db.collection(IOCONNECTION);
+  ioServer.adapter(createAdapter(mongoCollection));
+};
 const handleError = (error) => console.log("DB Error", error);
 
 db.on("error", handleError);
